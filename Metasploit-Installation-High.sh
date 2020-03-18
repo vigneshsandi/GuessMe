b007378730ae68ab2721567e5f1a9e2ba94fb251
vigneshsandi <48412245+vigneshsandi@users.noreply.github.com>
2020-03-18 11:28:36 +0530

    Add files via upload

---
+++ Metasploit-Installation-High.sh
@@ -0,0 +1,76 @@
+#!data/data/com.termux/files/usr/bin/bash
+#Coded By God-Of-Death(VS)
+red='\033[1;31m'
+green='\033[1;32m'
+blue='\033[1;34m'
+white='\033[0m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#Code Begins Dude....
+
+echo -e  "
+$blue    ██████╗ ██╗   ██╗███████╗███████╗███████╗███╗   ███╗███████╗
+$red   ██╔════╝ ██║   ██║██╔════╝██╔════╝██╔════╝████╗ ████║██╔════╝
+$blue   ██║  ███╗██║   ██║█████╗  ███████╗███████╗██╔████╔██║█████╗
+$red   ██║   ██║██║   ██║██╔══╝  ╚════██║╚════██║██║╚██╔╝██║██╔══╝
+$blue   ╚██████╔╝╚██████╔╝███████╗███████║███████║██║ ╚═╝ ██║███████╗
+$red    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝╚═╝     ╚═╝╚══════╝
+      "
+echo  -e "$green Coded By$white God-Of-Death(VS), $white"
+echo -e "$cyan Subscribe Our YoutubeChannel :$red GuessMe $cyan"
+echo -e "$pink NOTE :$red !!! Need Internet Connection To Install Metasploit(7.0 AND ABOVE ANDROID VERSIONS) !!! $pink"
+echo -e "$cyan Please Wait... Boss!!!,$red We are Preparing For Installing The$green Metasploit(7.0 AND ABOVE ANDROID VERSIONS)$cyan"
+echo -e "$green METASPLOIT INSTALLTION FOR$pink 7.0 AND ABOVE ANDROID VERSIONS $green"
+sleep 7.0
+clear
+apt-get update -y
+apt-get upgrade -y
+pkg install wget -y
+pkg install openssh -y
+pkg install python -y
+pkg install python2 -y
+pip install lolocat
+pkg install git -y
+pip install --upgrade pip
+pkg install nano -y
+pip install --upgrade pip
+clear
+termux-setup-storage
+echo " "
+echo " "
+pkg install ruby -y
+pkg install unstable-repo -y
+pkg install metasploit -y
+clear
+echo -e "$red >>>>>>>>>>>>>>>>$cyan METASPLOIT INSTALLATION IS COMPLETED$cyan <<<<<<<<<<<<<<<<<<$red"
+echo -e "$green WAIT A MINUTES...... $red DON'T CANCELL THE PROCESS $red"
+sleep 2.0
+clear
+cd $HOME
+cd ..
+cd usr
+cd opt
+mv metasploit-framework $HOME
+cd $HOME
+cd metasploit-framework
+wget https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt
+bash fix-ruby-bigdecimal.sh.txt
+echo -e "$green Starting$cyan Metasploit-Framework."
+clear
+echo -e "$green Starting$cyan Metasploit-Framework.."
+clear
+echo -e "$green Starting$cyan Metasploit-Framework..."
+clear
+echo -e "$green Starting$cyan Metasploit-Framework...."
+clear
+echo -e "$green Starting$cyan Metasploit-Framework....."
+clear
+echo -e "$green Starting$cyan Metasploit-Framework......"
+clear
+echo -e "$green Starting$cyan Metasploit-Framework......."
+clear
+echo -e "$green Starting$cyan Metasploit-Framework........"
+sleep 5.0
+pg_ctl -D $PREFIX/var/lib/postgresql start
+sleep 5.0
+./msfconsole
